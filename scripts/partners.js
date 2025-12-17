9c7a72e6f2e5f110b460c032f5d2f16777d26cea
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2025-12-17 09:54:47 +0200

    Remove Vivacom

--- scripts/partners.js
+++ scripts/partners.js
@@ -1,6 +1,5 @@
 export const partners = [
     { src: "./public/partners/encorp.png", name: "Encorp.ai" },
-    { src: "./public/partners/vivacom.png", name: "VIVACOM" },
     { src: "./public/partners/coca-cola.png", name: "Coca Cola HBC" },
     { src: "./public/partners/createx.png", name: "CreateX" },
     { src: "./public/partners/draft-kings.png", name: "Draft Kings" },
